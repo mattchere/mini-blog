9937c85be4ed99cf3433e7d62c755bff070944bb
infinitepure <matt.j.cheah@gmail.com>
2017-10-29 00:32:31 +1100

    Create blog post list view

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import url
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^blogs/$', views.BlogPostListView.as_view(), name='blogs')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,7 @@
+from .models import Blogger, BlogPost, Comment
+
 from django.shortcuts import render
+from django.views import generic
 
 
 def index(request):
@@ -6,3 +9,8 @@ def index(request):
     View function for the site homepage.
     """
     return render(request, 'index.html', context={})
+
+
+class BlogPostListView(generic.ListView):
+    model = BlogPost
+    paginate_by = 5
