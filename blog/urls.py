c0f002e7fb329b3c62dc838f8e2c6cfe2241bc5e
infinitepure <matt.j.cheah@gmail.com>
2017-11-01 00:09:20 +1100

    Added functionality to add comments onto blog posts

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     url(r'^bloggers/$', views.BloggerListView.as_view(), name='bloggers'),
     url(r'^(?P<pk>\d+)$', views.BlogPostDetailView.as_view(), name='blog-detail'),
     url(r'^blogger/(?P<pk>\d+)$', views.BloggerDetailView.as_view(), name='blogger-detail'),
+    url(r'^(?P<pk>\d+)/create/$', views.CommentCreate.as_view(), name="comment-create"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,11 @@
 from .models import Blogger, BlogPost, Comment
 
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 from django.views import generic
+from django.views.generic.edit import CreateView
+
 
 
 def index(request):
@@ -38,3 +42,30 @@ class BloggerDetailView(generic.DetailView):
     Generic view class for the detail view of each blogger.
     """
     model = Blogger
+
+
+class CommentCreate(LoginRequiredMixin, CreateView):
+    model = Comment
+    fields = ['text']
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super(CreateView, self).get_context_data(**kwargs)
+        # Get the blogger object from the "pk" URL parameter and add it to the context
+        context['blogger'] = get_object_or_404(Blogger, pk = self.kwargs['pk'])
+        return context
+
+
+    def form_valid(self, form):
+        """
+        Add author and associated blog to form data before setting it as valid (so it is saved to model)
+        """
+        #Add logged-in user as author of comment
+        form.instance.commenter = self.request.user
+        #Associate comment with blog based on passed id
+        form.instance.blog_post = get_object_or_404(BlogPost, pk = self.kwargs['pk'])
+        # Call super-class form validation behaviour
+        return super(CommentCreate, self).form_valid(form)
+
+    def get_success_url(self):
+        return reverse('blog-detail', args=[self.kwargs['pk']])
