9445aae992f29b27a3c10ec32d04031716c9f003
infinitepure <matt.j.cheah@gmail.com>
2017-11-07 00:12:18 +1100

    Add tests for blog post list view

--- blog/tests/test_views.py
+++ blog/tests/test_views.py
@@ -4,13 +4,14 @@ from django.contrib.auth.models import User
 from blog.models import Blogger, BlogPost, Comment
 
 from django.core.urlresolvers import reverse
+from datetime import datetime
 
 class BloggerListViewTest(TestCase):
     
     @classmethod
     def setUpTestData(cls):
-        number_of_blog_posts = 13
-        for blog_num in range(number_of_blog_posts):
+        number_of_bloggers = 13
+        for blog_num in range(number_of_bloggers):
             user = User.objects.create_user(username=str(blog_num), password='12345')
             Blogger.objects.create(user=user, bio='A')
 
@@ -33,3 +34,17 @@ class BloggerListViewTest(TestCase):
         self.assertEqual(resp.status_code, 200)
 
         self.assertTrue(len(resp.context['blogger_list']) == 13)
+
+class BlogPostListViewTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        number_of_blog_posts = 13
+        for blog_num in range(number_of_blog_posts):
+            user = User.objects.create_user(username=str(blog_num), password='12345')
+            blogger = Blogger.objects.create(user=user, bio='A')
+            BlogPost.objects.create(title=str(blog_num), post_date=datetime.today(), author=blogger, description='Test')
+        
+    def test_url_exists_at_desired_location(self):
+        resp = self.client.get('/blog/blogs/')
+        self.assertEqual(resp.status_code, 200)
